6368881e044428b5ce8cfdb55149bd627a8a1ee9
Shailesh Acharya <acharyashailesh8@gmail.com>
2023-06-17 15:41:16 +0545

    updated

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,15 +1,5 @@
-export const config = {
-  runtime: 'experimental-edge',
-}
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default async function (req) {
-  return new Response(
-    JSON.stringify({ name: 'John Doe' }),
-    {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }
-  )
+export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' })
 }
