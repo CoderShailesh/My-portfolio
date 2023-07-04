bd01ce87e5367ac2e0247bcee4bd06ad6437f598
Shailesh Acharya <acharyashailesh8@gmail.com>
2023-07-04 21:26:17 +0545

    change keywords

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -257,7 +257,7 @@ const NavBar = () => {
 
             <button
               onClick={() => setMode(mode === "light" ? "dark" : "light")}
-              className={`w-6 h-6 ml-3 flex items-center justify-center rounded-full p-1
+              className={`w-6 h-6 ml-4 flex items-center justify-center rounded-full p-1
       ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}
       `}
             >
